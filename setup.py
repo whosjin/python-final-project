682adb0943179f2972517fdd516233e540f55097
Jin Hu <102392954+whosjin@users.noreply.github.com>
2022-04-18 17:08:02 -0500

    changes to setup

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
+from setuptools import find_packages
 from setuptools import setup
+
+with open("README.md") as f:
+    long_description = f.read()
 
 setup(
     name='Final Project',
-    version='1.1',
-    packages=['pyqt5', 'pyqt5.ui', 'pyqt5.logic', 'league_manager'],
-    url='',
-    license='',
+    version='1.0.0',
+    packages=find_packages(),
+    url='https://github.com/whosjin/python-final-project',
     author='Jin Hu',
     author_email='jzh0196@auburn.edu',
-    description='Python (CPSC 4970) Final Project'
+    description='Python (CPSC 4970) Final Project',
+    long_description=long_description,
+    install_requires={
+        "PyQt5==5.15.6",
+        "setuptools==62.1.0",
+        "yagmail~=0.15.277",
+        "pyqt5-tools=5.15.4",
+        "keyring=23.5.0"
+    },
+    python_requires='>=3.9',
+    entry_points={
+        "console_scripts": [
+            "league_manager = pyqt5.logic.main_window:main"
+        ]
+    },
 )
